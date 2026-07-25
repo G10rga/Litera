b90c21ba9bba501592b9e4fffc0b800c6eda74f2
lizigigauri01 <lizi.gigaurite01@geolab.edu.ge>
2026-07-26 00:59:07 +0400

    armcemo

--- app.py
+++ app.py
@@ -1,26 +1,27 @@
 from flask import Flask, render_template
-
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', static_url_path='')
 
 
 @app.route('/')
+@app.route('/index.html')
 def index():
-    """Render the home page."""
     return render_template('index.html')
+
+
+@app.route('/work.html')
+def work():
+    return render_template('work.html')
 
 
 @app.route('/about')
 def about():
-    """Render the about page."""
     return render_template('about.html')
 
 
 @app.route('/contact')
 def contact():
-    """Render the contact page."""
     return render_template('contact.html')
 
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
-
