56db0a8fe8924db6a349b484e8d3e3b0a79242cd
G1orga <lasha4854@gmail.com>
2026-07-27 19:08:49 +0400

    Add Vefxistyaosani model and CSV upload functionality

--- models.py
+++ models.py
@@ -15,3 +15,20 @@ class Aphorism(db.Model):
     def __repr__(self):
         return f'<Aphorism {self.id}: {self.text[:50]}...>'
 
+
+class VefxistyaosaniLine(db.Model):
+    """Model for storing lines from Vefxistyaosani (The Knight in the Panther's Skin)."""
+    __tablename__ = 'vefxistyaosani_lines'
+
+    id = db.Column(db.Integer, primary_key=True)
+    line = db.Column(db.Text, nullable=False)
+    chapter = db.Column(db.String(255), nullable=True)
+    chapter_id = db.Column(db.Integer, nullable=True)
+    strophe_id = db.Column(db.Integer, nullable=True)
+    line_id = db.Column(db.Integer, nullable=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<VefxistyaosaniLine {self.id}: {self.line[:50]}...>'
+
+
